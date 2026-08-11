879c4bc21c0cd44657335550c56f0e547d3f595c
jazermoren-sketch <jazermoren@gmail.com>
2026-08-11 17:57:11 +0100

    Scope application stages by server

--- application/manager.py
+++ application/manager.py
@@ -1,111 +1,48 @@
-from application.storage import (
-    load_stages,
-    save_stages,
-)
+from application.storage import load_stages, save_stages
 
 
 class StageManager:
+    @staticmethod
+    def create_stage(guild_id, name: str):
+        stages = load_stages(guild_id)
+        if name in stages: return False
+        stages[name] = {"questions": []}; save_stages(guild_id, stages); return True
 
     @staticmethod
-    def create_stage(name: str):
-
-        stages = load_stages()
-
-        if name in stages:
-            return False
-
-        stages[name] = {
-            "questions": []
-        }
-
-        save_stages(stages)
-
-        return True
+    def delete_stage(guild_id, name: str):
+        stages = load_stages(guild_id)
+        if name not in stages: return False
+        del stages[name]; save_stages(guild_id, stages); return True
 
     @staticmethod
-    def delete_stage(name: str):
-
-        stages = load_stages()
-
-        if name not in stages:
-            return False
-
-        del stages[name]
-
-        save_stages(stages)
-
-        return True
+    def list_stages(guild_id): return load_stages(guild_id)
 
     @staticmethod
-    def list_stages():
-
-        return load_stages()
-
-    @staticmethod
-    def add_question(stage, question):
-
-        stages = load_stages()
-
-        if stage not in stages:
-            return False
-
-        stages[stage]["questions"].append(question)
-
-        save_stages(stages)
-
-        return True
+    def add_question(guild_id, stage, question):
+        stages = load_stages(guild_id)
+        if stage not in stages: return False
+        stages[stage]["questions"].append(question); save_stages(guild_id, stages); return True
 
     @staticmethod
-    def edit_question(stage, index, question):
-
-        stages = load_stages()
-
-        if stage not in stages:
-            return False
-
+    def edit_question(guild_id, stage, index, question):
+        stages = load_stages(guild_id)
+        if stage not in stages: return False
         questions = stages[stage]["questions"]
-
-        if index < 0 or index >= len(questions):
-            return False
-
-        questions[index] = question
-
-        save_stages(stages)
-
-        return True
+        if index < 0 or index >= len(questions): return False
+        questions[index] = question; save_stages(guild_id, stages); return True
 
     @staticmethod
-    def remove_question(stage, index):
-
-        stages = load_stages()
-
-        if stage not in stages:
-            return False
-
+    def remove_question(guild_id, stage, index):
+        stages = load_stages(guild_id)
+        if stage not in stages: return False
         questions = stages[stage]["questions"]
-
-        if index < 0 or index >= len(questions):
-            return False
-
-        questions.pop(index)
-
-        save_stages(stages)
-
-        return True
+        if index < 0 or index >= len(questions): return False
+        questions.pop(index); save_stages(guild_id, stages); return True
 
     @staticmethod
-    def get_questions(stage):
-
-        stages = load_stages()
-
-        if stage not in stages:
-            return []
-
-        return stages[stage]["questions"]
+    def get_questions(guild_id, stage):
+        stages = load_stages(guild_id)
+        return stages[stage]["questions"] if stage in stages else []
 
     @staticmethod
-    def stage_exists(stage):
-
-        stages = load_stages()
-
-        return stage in stages
+    def stage_exists(guild_id, stage): return stage in load_stages(guild_id)
